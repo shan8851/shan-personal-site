bc9b63faf9afdb4d31750be60a493b9fcc42a1b8
shan5742 <asamshan456@gmail.com>
2022-05-25 23:09:19 +0100

    adding project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,7 @@
 import mt from "../assets/mtgif.gif";
 import us from "../assets/upswing.png";
-import sp from "../assets/ga.png";
-//import rdg from "../assets/rdg.png";
+//import sp from "../assets/ga.png";
+import mac from '../assets/mac.png'
 import ss from "../assets/ss.png";
 import faucet from "../assets/faucet.png";
 import db from "../assets/dashboard.png";
@@ -24,6 +24,14 @@ export const projects = [
       "A Rinkeby faucet allowing devs to request and donate test Eth.",
     url: "https://rinkedry.vercel.app/",
     image: faucet,
+  },
+    {
+    id: uuidv4(),
+    title: "MAC-Insurance",
+    description:
+      "Insurance protocol to protect from ERC20 token price loss. Built as part of Ethglobal hackathon - multiple prize winner",
+    url: "https://showcase.ethglobal.com/hackmoney2022/mac-insurance-r1g12",
+    image: mac,
   },
   {
     id: uuidv4(),
@@ -55,11 +63,11 @@ export const projects = [
   //   url: "https://readydevgo.co/",
   //   image: rdg,
   // },
-  {
-    id: uuidv4(),
-    title: "Support Privacy",
-    description: "Privacy first alternatives for popular Google products",
-    url: "https://fuck-google.netlify.app/",
-    image: sp,
-  },
+  // {
+  //   id: uuidv4(),
+  //   title: "Support Privacy",
+  //   description: "Privacy first alternatives for popular Google products",
+  //   url: "https://fuck-google.netlify.app/",
+  //   image: sp,
+  // },
 ];
